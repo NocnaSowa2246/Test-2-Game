0661ec975567efd1d437aad4f2eb638829e773c2
XiroxNull <braden.griffiths@icloud.com>
2025-06-15 17:02:43 -0500

    Adding gun functionality

---
+++ Scripts/gun_script.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+var bullet = preload("res://Scene/bullet.tscn")
+var bullet_speed = 100
+@onready var gun_image = $Gun
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	look_at(get_global_mouse_position())
+	if Input.is_action_just_pressed("shoot"):
+		var bullet_instance = bullet.instantiate()
+		bullet_instance.position = global_position
+		bullet_instance.rotation_degrees = rotation_degrees
+		#bullet_instance.apply_impulse(Vector2(), Vector2(bullet_speed, 0).rotated(rotation))
+		get_tree().get_root().add_child(bullet_instance)

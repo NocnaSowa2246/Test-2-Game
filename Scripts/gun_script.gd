4b1fee230a96e5a068e418c8a3ecc1961b8d8002
NocnaSowa2246 <isaacdean2010@gmail.com>
2025-06-15 15:52:37 -0700

    Update gun_script.gd

--- Scripts/gun_script.gd
+++ Scripts/gun_script.gd
@@ -1,15 +1,26 @@
 extends Node2D
 
+<<<<<<< Updated upstream
 var bullet = preload("res://Scene/bullet.tscn")
 var bullet_speed = 1000
 var bullet_instance = bullet.instantiate()
+=======
+var _bullet = preload("res://Scene/bullet.tscn")
+var bullet_speed = 100
+
+>>>>>>> Stashed changes
 @onready var gun_image = $Gun
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	look_at(get_global_mouse_position())
 	if Input.is_action_just_pressed("shoot"):
+<<<<<<< Updated upstream
 		bullet_instance = bullet.instantiate()
+=======
+		var bullet_instance = bullet.instantiate()
+
+>>>>>>> Stashed changes
 		bullet_instance.position = global_position
 		bullet_instance.rotation_degrees = rotation_degrees
 		bullet_instance.velocity = Vector2(bullet_speed, 0).rotated(rotation)

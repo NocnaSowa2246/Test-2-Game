b8b4c96defae10208d0b32460ea7f5a5ef5f0581
XiroxNull <braden.griffiths@icloud.com>
2025-06-16 16:34:27 -0500

    Grapple (half working)

--- Scripts/gun_script.gd
+++ Scripts/gun_script.gd
@@ -3,8 +3,7 @@ extends Node2D
 var bullet = preload("res://Scene/bullet.tscn")
 var bullet_speed = 1000
 var bullet_instance = bullet.instantiate()
-var _bullet = preload("res://Scene/bullet.tscn")
-var mode = 0
+var mode = 1
 @onready var gun_image = $Gun
 var ammo = 20 #bullets in magazine
 var stash = 50 #Bullets total.
@@ -14,12 +13,12 @@ var hook = 1
 func _process(delta: float) -> void:
 	look_at(get_global_mouse_position())
 	if Input.is_action_just_pressed("reload"):
-		ammo=0 #Supid for it to work, but it works.
+		ammo = 0 #Supid way for it to work, but it works.
 		reload()
 	if Input.is_action_just_pressed("swap"):
 		swapper()
 	if ammo>0:
-		if Input.is_action_just_pressed("shoot") and mode==1:
+		if Input.is_action_just_pressed("shoot") and mode == 1:
 			bullet_instance = bullet.instantiate()
 			var bullet_instance = bullet.instantiate()
 			bullet_instance.position = global_position
@@ -28,26 +27,27 @@ func _process(delta: float) -> void:
 			get_tree().get_root().add_child(bullet_instance)
 			ammo = ammo - 1
 			stash = stash - 1
-	if Input.is_action_just_pressed("shoot") and mode==0 and hook==1:
-		hook=0
-		var hook_instance = bullet.instantiate()
-		hook_instance.position = global_position
-		hook_instance.rotation_degrees = rotation_degrees
-		hook_instance.velocity = Vector2(bullet_speed, 0).rotated(rotation)
-		get_tree().get_root().add_child(hook_instance)
-		await get_tree().create_timer(3.0).timeout #Might want to change later
-		hook=1
+			#
+	#if Input.is_action_just_pressed("shoot") and mode==0 and hook==1:
+		#hook=0
+		#var hook_instance = bullet.instantiate()
+		#hook_instance.position = global_position
+		#hook_instance.rotation_degrees = rotation_degrees
+		#hook_instance.velocity = Vector2(bullet_speed, 0).rotated(rotation)
+		#get_tree().get_root().add_child(hook_instance)
+		#await get_tree().create_timer(3.0).timeout #Might want to change later
+		#hook = 1
 
 func reload()-> void:
 	await get_tree().create_timer(3.0).timeout
 	if stash<20:
-		ammo=stash
+		ammo = stash
 	else:
-		ammo=20
+		ammo = 20
 
 func swapper()->void: #Swaps between gun and hook (Hook features not added yet)
 	if Input.is_action_just_pressed("swap"):
-		if mode==0:
-			mode=1
-		if mode==1:
-			mode=0
+		if mode == 0:
+			mode = 1
+		elif mode == 1:
+			mode = 0

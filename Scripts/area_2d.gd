be5822462589053c7d6548649edb31fc619af856
XiroxNull <braden.griffiths@icloud.com>
2025-06-16 20:42:12 -0500

    Grapple Tinkering

--- Scripts/area_2d.gd
+++ Scripts/area_2d.gd
@@ -3,4 +3,4 @@ extends Area2D
 var speed = 1000
 
 func _physics_process(delta):
-	position.x+= speed * delta
+	position.x += speed * delta
--- Scripts/gun_script.gd
+++ Scripts/gun_script.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var bullet = preload("res://Scene/bullet.tscn")
+var hook = preload("res://Scene/hook.tscn")
 var bullet_speed = 1000
 var bullet_instance = bullet.instantiate()
 var mode = 1
@@ -8,7 +9,7 @@ var mode = 1
 @onready var tip = $Grapple
 var ammo = 20 #bullets in magazine
 var stash = 50 #Bullets total.
-var hook = 1
+var hook_p = 1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -29,15 +30,18 @@ func _process(delta: float) -> void:
 			ammo = ammo - 1
 			stash = stash - 1
 			
-	if Input.is_action_just_pressed("shoot") and mode==0 and hook==1:
-		hook=0
-		var hook_instance = bullet.instantiate()
-		hook_instance.position = global_position
+	if Input.is_action_just_pressed("shoot") and mode==0 and hook_p == 1:
+		hook_p = 0
+		var hook_instance = hook.instantiate()
+		hook_instance.position = tip.global_position
 		hook_instance.rotation_degrees = rotation_degrees
 		hook_instance.velocity = Vector2(bullet_speed, 0).rotated(rotation)
 		get_tree().get_root().add_child(hook_instance)
 		await get_tree().create_timer(3.0).timeout #Might want to change later
-		hook = 1
+		hook_p = 1
+		var ray_point = hook_instance.get_collision_point()
+		if ray_point:
+			hook_instance.velocity = Vector2(0, 0).rotated(rotation)
 
 func reload()-> void:
 	await get_tree().create_timer(3.0).timeout

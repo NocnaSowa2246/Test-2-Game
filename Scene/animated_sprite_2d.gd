10c532c8a88cf33711d90080b4abcff7ee4ab78f
XiroxNull <braden.griffiths@icloud.com>
2025-06-17 12:46:14 -0500

    Revert "An extremely basic pole to start"
    This reverts commit cfcb09dc6eccf5c15382c59641884b1af5c03fc9.

--- Scene/animated_sprite_2d.gd
+++
@@ -1,7 +0,0 @@
-extends AnimatedSprite2D
-
-@onready var animated_sprite_2d: AnimatedSprite2D = $"."
-var AnimatedSprite: AnimatedSprite2D
-
-func _process(delta):
- AnimatedSprite2D.rotation += deg_to_rad(2)
--- Scene/steve_2.gd
+++
@@ -1,6 +0,0 @@
-extends Sprite2D
-
-@onready var steve_2: Sprite2D = $"."
-
-func _process(delta):
-	steve_2.rotation += deg_to_rad(2)
